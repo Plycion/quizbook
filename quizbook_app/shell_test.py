f3d9f35e34a8a702800614d223bf6fedd82fc54f
Plycion <plycion@gmail.com>
2014-05-22 15:10:02 -0400

    updated shell test

--- quizbook_app/shell_test.py
+++ quizbook_app/shell_test.py
@@ -1,3 +1,4 @@
+import sys
 from quizbook_app.models import *
 from django.utils import timezone
 
@@ -9,56 +10,50 @@ c2 = Course(name="c2", pub_date=timezone.now())
 c1.save()
 c2.save()
 
-q1 = Quiz(course=c1, question="", answer="", pub_date=timezone.now())
-q2 = Quiz(course=c2, question="", answer="", pub_date=timezone.now())
+c1.create_quiz(question="What is 2+2", answer="4", creator=user)
+c2.create_quiz(question="What is 3+2", answer="5", creator=user)
 
-q1.save()
-q2.save()
+# p = Practice(user=user)
 
+# p.save()
 
-r1 = QuizRecord.objects.create_quiz_record(quiz=q1, user=user)
-r2 = QuizRecord.objects.create_quiz_record(quiz=q2, user=user)
+# p.add_quiz_record(r1)
+# p.add_quiz_record(r2)
 
-r1.save()
-r2.save()
+# t1 = RecordToken.objects.get(quiz_record=r1)
+# t2 = RecordToken.objects.get(quiz_record=r2)
 
-p = Practice(user=user)
+# t1.weight = 0
+# t2.weight = 10
 
-p.save()
-
-p.add_quiz_record(r1)
-p.add_quiz_record(r2)
-
-t1 = RecordToken.objects.get(quiz_record=r1)
-t2 = RecordToken.objects.get(quiz_record=r2)
-
-t1.weight = 0
-t2.weight = 10
-
-t1.save()
-t2.save()
+# t1.save()
+# t2.save()
 
 
-p.save()
+# p.save()
 
-print "Records: %s" % str(RecordToken.objects.filter(practice=p))
+# print "Records: %s" % str(RecordToken.objects.filter(practice=p))
 
 
-print p.is_empty()
+# print p.is_empty()
 
-quiz = p.pop_quiz()
-print str(quiz)
+# quiz = p.pop_quiz()
+# print str(quiz)
 
-quiz = p.pop_quiz()
-print str(quiz)
+# quiz = p.pop_quiz()
+# print str(quiz)
 
-p.delete()
+# arg = sys.argv[1]
 
-r1.delete()
-r2.delete()
+# agr = "-d"
+# if arg == "-d":
+# 	p.delete()
 
-q1.delete()
-q2.delete()
+# 	r1.delete()
+# 	r2.delete()
 
-c1.delete()
-c2.delete()
+# 	q1.delete()
+# 	q2.delete()
+
+# 	c1.delete()
+# 	c2.delete()
