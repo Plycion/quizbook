2ca30790e22ee1fc8927d630b0cafe7779709a67
plycion <plycion@gmail.com>
2014-07-20 23:16:08 -0400

    modified start database script to account for new settings env

--- quizbook_app/start_database.sh
+++ quizbook_app/start_database.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo 'yes\nplycion\na@a.com\n' > start_database_input.in
-python manage.py syncdb < start_database_input.in
+python manage.py syncdb --settings="quizbook.settings.development" < start_database_input.in
 
 
 echo "execfile('quizbook_app/shell_test.py')\n" > start_database_input.in
-python manage.py migrate quizbook_app
-python manage.py shell < start_database_input.in
+python manage.py migrate quizbook_app --settings="quizbook.settings.development"
+python manage.py shell --settings="quizbook.settings.development" < start_database_input.in
 
 rm start_database_input.in
 open 'http://127.0.0.1:8000'
-python manage.py runserver
+python manage.py runserver --settings="quizbook.settings.development"
