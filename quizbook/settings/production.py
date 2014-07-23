8c4a808efaf442a44bf5605d09ec55d80fe54ce2
plycion <plycion@gmail.com>
2014-07-23 14:50:01 -0400

    production settings modified

--- quizbook/settings/production.py
+++ quizbook/settings/production.py
@@ -23,10 +23,10 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
